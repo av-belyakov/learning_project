05935b1c434b10d7a9bd7f165b6ba3ad36e34372
Artemij Belyakov <bart82@mail.ru>
2023-11-01 16:03:03 +0300

    01.11.2023 	modified:   ui/static/js/common.js 	modified:   ui/static/js/main.js 	new file:   ui/static/js/manifest.webmanifest 	modified:   ui/static/js/page_photo/main.js 	modified:   ui/static/js/styles.js 	modified:   ui/views/base.html 	modified:   ui/views/music.html 	modified:   ui/views/photo.html 	modified:   ui/views/useful_notes.html 	modified:   webpack.config.js

--- ui/static/js/page_photo/main.js
+++ ui/static/js/page_photo/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
-import myImg1 from '../../img/maxresdefault.jpg';
+// import imgTmp1 from '../../img/maxresdefault.jpg';
+import imgTmp1 from '../../../static/img/maxresdefault.jpg';
 
 (function() {
   const img1 = document.getElementById('img_1');
 
   const myNewImg1 = document.createElement('img');
-  myNewImg1.setAttribute('src', '../../img/maxresdefault.jpg');
-  img1.insertAdjacentElement(myNewImg1);
+  // myNewImg1.setAttribute('src', 'dist/_/img/maxresdefault.jpg');
+  myNewImg1.setAttribute('src', imgTmp1);
+
+  console.log(myNewImg1);
+  console.log(imgTmp1);
+
+  img1.insertAdjacentElement('afterend', myNewImg1);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,50 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-let bootstrapCSS = path.join(__dirname, "node_modules/bootstrap/dist/css");
+const bootstrapCSS = path.join(__dirname, 'node_modules/bootstrap/dist/css');
 
 module.exports = {
-    //место откуда берутся js файлы
-    context: __dirname + "/ui/static/js",
+  // место откуда берутся js файлы
+  context: __dirname + '/ui/static/js',
 
-    //точки входа
-    entry: {
-        vendors: [
-            "bootstrap",
-        ],
-        pagePhoto: "./page_photo/main.js",
-        //index: "./index.js",
-        common: "./common.js",
-        styles: "./styles.js",
+  // точки входа
+  entry: {
+    vendors: [
+      'bootstrap',
+    ],
+    common: './common.js',
+    styles: './styles.js',
+    mainBase: './main.js',
+    pagePhoto: './page_photo/main.js',
+  },
+
+  output: {
+    // вывод обработанных webpack js файлов в указанную директорию
+    path: path.resolve(__dirname + '/ui', 'dist'),
+
+    // интернет путь до указанной директории
+    publicPath: '/dist/',
+
+    // шаблон для формируемых webpack файлов
+    filename: '[name].bundle.js',
+
+    // применяется при сборке файлов через require.ensure ПОКА НЕ ЗНАЮ ДЛЯ ЧЕГО
+    chunkFilename: '[id].js',
+
+    // экспорт каждой точки входа должен попадать в переменную с соответствующем
+    // именем  ПОКА НЕ ЗНАЮ ДЛЯ ЧЕГО
+    library: '[name]',
+  },
+
+  resolve: {
+    modules: ['node_modules', bootstrapCSS],
+    extensions: ['', 'js', 'jsx', 'css'],
+    alias: {
+      'bootstrap': 'bootstrap/dist/js/bootstrap.min.js',
     },
+  },
 
-    output: {
-        //вывод обработанных webpack js файлов в указанную директорию
-        path: path.resolve(__dirname + '/ui', 'dist'),
-
-        //интернет путь до указанной директории
-        publicPath: "/ui/dist/",
-
-        //шаблон для формируемых webpack файлов
-        filename: '[name].bundle.js',
-
-        //применяется при сборке файлов через require.ensure (ВОТ ЭТО ПОКА НЕ ЗНАЮ ДЛЯ ЧЕГО)
-        chunkFilename: "[id].js",
-
-        //экспорт каждой точки входа должен попадать в переменную с соответствующем именем (ВОТ ЭТО ПОКА НЕ ЗНАЮ ДЛЯ ЧЕГО)
-        library: "[name]"
-    },
-
-    resolve: {
-        modules: ["node_modules", bootstrapCSS],
-        extensions: ["", "js", "jsx", "css"],
-        alias: {
-            "bootstrap": "bootstrap/dist/js/bootstrap.min.js",
-        }
-    },
-
-    /*
+  /*
     resolveLoader: {
         modules: ["node_modules"],
         extensions: [".js"],
@@ -51,11 +52,11 @@ module.exports = {
     },
     */
 
-    plugins: [
-        //не собирать если есть ошибки
-        new webpack.NoEmitOnErrorsPlugin(),
+  plugins: [
+    // не собирать если есть ошибки
+    new webpack.NoEmitOnErrorsPlugin(),
 
-        /*
+    /*
         //выносит все стили в отдельные файлы ВРОДЕ ДЛЯ webpack 5 НЕ НУЖНО
         new ExtractTextPlugin("css/[id]_[name].css", { allChunks: true }),
 
@@ -68,37 +69,35 @@ module.exports = {
 
         new webpack.optimize.OccurrenceOrderPlugin(true),
         */
-    ],
+  ],
 
-    module: {
-        rules: [
-            {
-                //test: /\.m?js$/,
-                test: /\.(js|jsx)$/, // определяем тип файлов
-                exclude: /node_modules/, // исключаем из обработки папку node_modules
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                    },
-                },
-            },
-            {
-                test: /\.css$/i,
-                exclude: /node_modules/, // исключаем из обработки папку node_modules
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?.*)?$/,
-                exclude: /\/node_modules\//,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "[path][name].[ext]",
-                        //publicPath: "dist/images",
-                    },
-                }],
-            }
-        ],
-    }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // определяем тип файлов
+        exclude: /node_modules/, // исключаем из обработки папку node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/, // исключаем из обработки папку node_modules
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?.*)?$/,
+        exclude: /\/node_modules\//,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
 };
